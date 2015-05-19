26708fba3257f05041a851d5582af6aeee2a4314
jstikeleather <jenny.stikeleather@gmail.com>
2015-05-19 01:00:43 -0400

    Completed Grocery List App/Tutorial 4-4.2

---
+++ GroceryListApp/ShoppingListCell.swift
@@ -0,0 +1,26 @@
+//
+//  ShoppingListCell.swift
+//  GroceryListApp
+//
+//  Created by Jennifer Stikeleather on 5/14/15.
+//  Copyright (c) 2015 Jennifer Stikeleather. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListCell: UITableViewCell {
+    
+    @IBOutlet weak var labelName: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
